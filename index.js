aeca43a46d4455bd3e45d8cacc2344601394905b
mudalib03 <mudalibabdulle17@gmail.com>
2025-03-04 12:43:51 +0300

    variables lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+
+const companyName = 'Scuber';
+
+
+let mostProfitableNeighborhood = "Chelsea";
+console.log(mostProfitableNeighborhood);
+
+let companyCeo = `Susan Smith`;
